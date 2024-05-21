e3932f4b5afd07890f872e85ec0902104476342b
simony05 <67559452+simony05@users.noreply.github.com>
2024-05-20 18:59:22 -0700

    set up fetch

--- app/src/components/RecipeScreen.js
+++ app/src/components/RecipeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, FlatList} from 'react-native';
 
 export const RecipeScreen = ({ route }) => {
@@ -9,6 +9,18 @@ export const RecipeScreen = ({ route }) => {
             <Text>- {item}, {index}</Text>
         </View>
     );
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://169.231.126.186:8081/recipe?ingredients= pasta tomato onion', {
+            method: 'GET'
+        })
+        .then(response => response.json())
+        .then(recipes => {
+            console.log(recipes)
+            //setData(recipes)
+        })
+    }, [])
 
     return (
         <View>
