8bd7a9199c63506af6deb30c9baa498397f92d14
simony05 <67559452+simony05@users.noreply.github.com>
2024-06-30 18:19:20 -0700

    card edits

--- app/src/components/RecipeScreen.js
+++ app/src/components/RecipeScreen.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Keyboard } from 'react-native';
 import { RecipeCard } from '../features/RecipeCard';
 
 export const RecipeScreen = ({ route }) => {
 
     const [isLoading, setIsLoading] = useState(false);
-    const { history } = route.params || [];
+    const [ingredients, setIngredients] = useState([]);
+    //const { history } = route.params || [];
+    setIngredients(route.params);
+
     const renderItem = ({ item, index }) => (
         <View>
             <Text>- {item}, {index}</Text>
@@ -14,21 +17,17 @@ export const RecipeScreen = ({ route }) => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        setIsLoading(true);
-        const params = history.join(' ');
-        fetch(`http://10.0.0.119:5000/recipe?ingredients=${params}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(response => response.json())
-        .then(recipes => {
-            const formattedData = Object.values(recipes)
-            setData(formattedData)
+        const fetchData = async () => {
+            setIsLoading(true);
+            const params = ingredients.join(' ');
+            const response = await fetch(`http://10.0.0.119:5000/recipe?ingredients=${params}`);
+            const recipes = await response.json();
+            //const formattedData = Object.values(recipes);
+            setData( Object.values(recipes));
             setIsLoading(false);
-        })
-    }, [])
+        };
+        fetchData();
+    }, [ingredients])
 
     return (
         <View>
@@ -36,7 +35,7 @@ export const RecipeScreen = ({ route }) => {
             {isLoading ? <Text>Loading...</Text> : null}
             <FlatList 
                 onScrollBeginDrag={() => Keyboard.dismiss()}
-                data={history} 
+                data={ingredients} 
                 renderItem={renderItem} 
                 keyExtractor={(item, index) => index.toString()}
             />
--- app/src/features/RecipeCard.js
+++ app/src/features/RecipeCard.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 export const RecipeCard = ({ recipe }) => {
     return (
         <View>
             <Text>{recipe.title}</Text>
+            <Image
+                source={{ uri: `data:image/jpeg;base64,${recipe.image}` }}
+                style={{ width: 200, height: 200 }}
+            />
         </View>
     );
 };
